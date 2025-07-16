2e9c835c9d137b71a297eb8c7aca683661258087
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2025-07-16 23:31:44 +0200

    Initial Commit

---
+++ Cydia Repo Downloader SwiftUI/Cydia_Repo_Downloader_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cydia_Repo_Downloader_SwiftUIApp.swift
+//  Cydia Repo Downloader SwiftUI
+//
+//  Created by Victor on 16.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Cydia_Repo_Downloader_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
